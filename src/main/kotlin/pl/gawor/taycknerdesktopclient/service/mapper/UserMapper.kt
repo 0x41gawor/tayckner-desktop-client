f472798005bb425ec534e3a315face5edc77efb6
0x41gawor <41gawor@gmail.com>
2022-02-23 22:43:29 +0100

    feat(service): add user mapper interface #5

--- src/main/kotlin/pl/gawor/taycknerdesktopclient/service/mapper/UserMapper.kt
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/service/mapper/UserMapper.kt
@@ -3,13 +3,14 @@ package pl.gawor.taycknerdesktopclient.service.mapper
 import pl.gawor.taycknerdesktopclient.model.User
 import pl.gawor.taycknerdesktopclient.repository.entity.UserEntity
 
-class UserMapper {
-    fun modelToEntity(model: User?) : UserEntity {
+class UserMapper : IMapper<User, UserEntity> {
+
+    override fun modelToEntity(model: User?) : UserEntity {
         if (model == null) return UserEntity()
         return UserEntity(model.id, model.username, model.password, model.firstName, model.lastName, model.email)
     }
 
-    fun entityToModel(entity: UserEntity?) : User? {
+    override fun entityToModel(entity: UserEntity?) : User? {
         if (entity == null) return null
         return User(entity.id, entity.username, entity.password, entity.firstName, entity.lastName, entity.email)
     }
