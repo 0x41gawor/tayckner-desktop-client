c3e0c5a1385e1d795e0011672d14d67216ef2872
0x41gawor <41gawor@gmail.com>
2022-02-23 22:43:38 +0100

    feat(service): add schedule mapper interface #5

---
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/service/mapper/ScheduleMapper.kt
@@ -0,0 +1,30 @@
+package pl.gawor.taycknerdesktopclient.service.mapper
+
+import pl.gawor.taycknerdesktopclient.model.Schedule
+import pl.gawor.taycknerdesktopclient.model.User
+import pl.gawor.taycknerdesktopclient.repository.UserRepository
+import pl.gawor.taycknerdesktopclient.repository.entity.ScheduleEntity
+import pl.gawor.taycknerdesktopclient.repository.entity.UserEntity
+import pl.gawor.taycknerdesktopclient.service.Service
+
+
+class ScheduleMapper : IMapper<Schedule, ScheduleEntity> {
+
+    private val userRepository = UserRepository()
+    private val userMapper = UserMapper()
+    private val userService = Service<User, UserEntity>(userRepository, userMapper)
+
+    override fun modelToEntity(model: Schedule?) : ScheduleEntity {
+        if (model == null) return ScheduleEntity()
+        return ScheduleEntity(model.id, model.name, model.startTime, model.endTime, model.duration, model.user.id)
+    }
+
+    override fun entityToModel(entity: ScheduleEntity?) : Schedule? {
+        if (entity == null) return null
+        var user = userService.read(entity.userId)
+        if (user == null) {
+            user = User()
+        }
+        return Schedule(entity.id, entity.name, entity.startTime, entity.endTime, entity.duration, user)
+    }
+}
