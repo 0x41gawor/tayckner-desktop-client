7099f804c3b1fe5cacbc4e85ed0cbec748bb1bf3
0x41gawor <41gawor@gmail.com>
2022-02-23 22:43:51 +0100

    feat(service): add generic service #5

---
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/service/Service.kt
@@ -0,0 +1,44 @@
+package pl.gawor.taycknerdesktopclient.service
+
+import pl.gawor.taycknerdesktopclient.repository.ICrudRepository
+import pl.gawor.taycknerdesktopclient.service.mapper.IMapper
+
+class Service<Model, Entity>(
+    private val repository: ICrudRepository<Entity>,
+    private val mapper: IMapper<Model, Entity>)
+    : ICrudService<Model> {
+
+    override fun list(): List<Model>? {
+        val entities = repository.list()
+        val models  = ArrayList<Model>()
+        if (entities != null) {
+            for (entity in entities) {
+                val model = mapper.entityToModel(entity)
+                models.add(model!!)
+            }
+            return models
+        }
+        return null
+    }
+
+    override fun create(model: Model): Model? {
+        val entity = mapper.modelToEntity(model)
+        val createdEntity = repository.create(entity)
+        return mapper.entityToModel(createdEntity)
+    }
+
+    override fun read(id: Int): Model? {
+        val readEntity = repository.read(id)
+        return mapper.entityToModel(readEntity)
+    }
+
+    override fun update(id: Int, model: Model): Model? {
+        val entity = mapper.modelToEntity(model)
+        val updatedEntity = repository.update(id, entity)
+        return mapper.entityToModel(updatedEntity)
+    }
+
+    override fun delete(id: Int): Boolean {
+        return repository.delete(id)
+    }
+}
