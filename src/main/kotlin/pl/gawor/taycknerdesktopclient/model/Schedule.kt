964ab8a54533dea3f1f7e60835d7c8a3088a1880
0x41gawor <41gawor@gmail.com>
2022-02-23 16:21:37 +0100

    feat(model): add Schedule model class #1

---
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/model/Schedule.kt
@@ -0,0 +1,14 @@
+package pl.gawor.taycknerdesktopclient.model
+
+import java.time.LocalDateTime
+
+data class Schedule(
+    val id: Int,
+    val name: String,
+    val startTime: LocalDateTime,
+    val endTime: LocalDateTime,
+    val duration: Double,
+    val user: User
+) {
+    constructor() : this(0, "", LocalDateTime.MIN, LocalDateTime.MIN, 0.0, User())
+}
