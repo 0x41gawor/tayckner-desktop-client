cbcc81d2358b49967be0b477840ae06ce919e8fc
0x41gawor <41gawor@gmail.com>
2022-02-25 23:07:03 +0100

    refactor(dp-model): refactor model #10

--- src/main/kotlin/pl/gawor/taycknerdesktopclient/model/Schedule.kt
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/model/Schedule.kt
@@ -1,14 +1,16 @@
 package pl.gawor.taycknerdesktopclient.model
 
-import java.time.LocalDateTime
+import java.time.LocalDate
+import java.time.LocalTime
 
 data class Schedule(
     val id: Int,
     val name: String,
-    val startTime: LocalDateTime?,
-    val endTime: LocalDateTime?,
+    val startTime: LocalTime?,
+    val endTime: LocalTime?,
+    val date: LocalDate,
     val duration: Double?,
     val user: User
 ) {
-    constructor() : this(0, "", LocalDateTime.MIN, LocalDateTime.MIN, 0.0, User())
+    constructor() : this(0, "", LocalTime.MIN, LocalTime.MIN, LocalDate.now(), 0.0, User())
 }
