31e04bfbb16d6839dcdd24219b6227082c453e9c
0x41gawor <41gawor@gmail.com>
2022-02-25 20:41:42 +0100

    feat(dp-service): add logger to ScheduleMapper #9

--- src/main/kotlin/pl/gawor/taycknerdesktopclient/service/mapper/ScheduleMapper.kt
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/service/mapper/ScheduleMapper.kt
@@ -15,16 +15,22 @@ class ScheduleMapper : IMapper<Schedule, ScheduleEntity> {
     private val userService = Service<User, UserEntity>(userRepository, userMapper)
 
     override fun modelToEntity(model: Schedule?): ScheduleEntity {
-        if (model == null) return ScheduleEntity()
-        return ScheduleEntity(model.id, model.name, model.startTime, model.endTime, model.duration, model.user.id)
+        println("ScheduleMapper.modelToEntity(model = $model)")
+        val result: ScheduleEntity = if (model == null) ScheduleEntity() else ScheduleEntity(model.id, model.name, model.startTime, model.endTime, model.duration, model.user.id)
+        println("ScheduleMapper.modelToEntity(model = $model) = $result")
+        return result
     }
 
     override fun entityToModel(entity: ScheduleEntity?): Schedule? {
-        if (entity == null) return null
-        var user = userService.read(entity.userId)
-        if (user == null) {
-            user = User()
+        println("ScheduleMapper.entityToModel(model = $entity)")
+        val result: Schedule? = if (entity == null) null else {
+            var user = userService.read(entity.userId)
+            if (user == null) {
+                user = User()
+            }
+            Schedule(entity.id, entity.name, entity.startTime, entity.endTime, entity.duration, user)
         }
-        return Schedule(entity.id, entity.name, entity.startTime, entity.endTime, entity.duration, user)
+        println("ScheduleMapper.entityToModel(model = $entity) = $result")
+        return result
     }
 }
