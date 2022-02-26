217c6e4d11e72622001aa79f67a1c43271f5396b
0x41gawor <41gawor@gmail.com>
2022-02-26 23:53:18 +0100

    feat(dt-view): create day-tracker pure layout #16

--- src/main/kotlin/pl/gawor/taycknerdesktopclient/TaycknerApplication.kt
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/TaycknerApplication.kt
@@ -7,7 +7,7 @@ import javafx.stage.Stage
 
 class TaycknerApplication : Application() {
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(TaycknerApplication::class.java.getResource("view/view.fxml"))
+        val fxmlLoader = FXMLLoader(TaycknerApplication::class.java.getResource("view/day_tracker/view.fxml"))
         val scene = Scene(fxmlLoader.load(), 1920.0, 1013.0)
         stage.title = "Tayckner"
         stage.scene = scene
