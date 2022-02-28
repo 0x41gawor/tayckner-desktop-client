e42fb3e1338a9965f7d8ba6a46f030a3c4b646bc
0x41gawor <41gawor@gmail.com>
2022-02-28 22:07:11 +0100

    feat(dt-controller): implement refresh acitvity list #26

--- src/main/kotlin/pl/gawor/taycknerdesktopclient/controller/DayTrackerController.kt
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/controller/DayTrackerController.kt
@@ -4,25 +4,30 @@ import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
 import javafx.geometry.Insets
-import javafx.scene.control.Button
-import javafx.scene.control.TextArea
-import javafx.scene.control.TextField
+import javafx.scene.control.*
 import javafx.scene.layout.GridPane
+import javafx.scene.layout.HBox
 import javafx.scene.layout.Region
 import javafx.scene.layout.VBox
 import pl.gawor.taycknerdesktopclient.TaycknerApplication
 import pl.gawor.taycknerdesktopclient.controller.Observer.ISubscriber
+import pl.gawor.taycknerdesktopclient.model.Activity
 import pl.gawor.taycknerdesktopclient.model.Category
 import pl.gawor.taycknerdesktopclient.model.User
+import pl.gawor.taycknerdesktopclient.repository.ActivityRepository
 import pl.gawor.taycknerdesktopclient.repository.CategoryRepository
 import pl.gawor.taycknerdesktopclient.repository.entity.CategoryEntity
+import pl.gawor.taycknerdesktopclient.service.ActivityService
 import pl.gawor.taycknerdesktopclient.service.Service
+import pl.gawor.taycknerdesktopclient.service.mapper.ActivityMapper
 import pl.gawor.taycknerdesktopclient.service.mapper.CategoryMapper
 import java.net.URL
 import java.util.*
 import kotlin.collections.ArrayList
 
 class DayTrackerController : Initializable, ISubscriber<Category> {
+
+    // C A T E G O R Y
 
     @FXML private lateinit var button_categoryDelete: Button
 
@@ -36,18 +41,55 @@ class DayTrackerController : Initializable, ISubscriber<Category> {
 
     @FXML private lateinit var textField_categoryName: TextField
 
+    // A C T I V I T Y
+
+    @FXML private lateinit var butoon_prev: Button
+
+    @FXML private lateinit var button_activity_add: Button
+
+    @FXML private lateinit var button_activity_delete: Button
+
+    @FXML private lateinit var button_next: Button
+
+    @FXML private lateinit var comboBox_activiy_category: ComboBox<Any>
+
+    @FXML private lateinit var gridPane_activity: GridPane
+
+    @FXML private lateinit var label_date: Label
+
+    @FXML private lateinit var textField_activity_breaks: TextField
+
+    @FXML private lateinit var textField_activity_endTime: TextField
+
+    @FXML private lateinit var textField_activity_name: TextField
+
+    @FXML private lateinit var textField_activity_startTime: TextField
+
+
     private var categories = ArrayList<Category>()
+    private var activities = ArrayList<Activity>()
 
     private lateinit var categoryService: Service<Category, CategoryEntity>
+    private lateinit var activityService: ActivityService
 
     private var selectedItemCategory: Category? = null
+
+    private val activityListener = object : ISubscriber<Activity> {
+        override fun update(model: Activity) {
+            println("elo")
+        }
+    }
 
     override fun initialize(p0: URL?, p1: ResourceBundle?) {
         val categoryRepository = CategoryRepository()
         val categoryMapper = CategoryMapper()
         categoryService = Service(categoryRepository, categoryMapper)
+        val activityRepository = ActivityRepository()
+        val activityMapper = ActivityMapper()
+        activityService = ActivityService(activityRepository, activityMapper)
 
         refreshCategoriesList()
+        refreshActivitiesList()
     }
 
     private fun refreshCategoriesList() {
@@ -71,6 +113,30 @@ class DayTrackerController : Initializable, ISubscriber<Category> {
             gridPane_category.prefWidth = Region.USE_COMPUTED_SIZE
             gridPane_category.prefHeight = Region.USE_COMPUTED_SIZE
             GridPane.setMargin(vbox, Insets(5.0))
+        }
+    }
+
+    private fun refreshActivitiesList() {
+        activities = activityService.list() as ArrayList<Activity>
+
+        gridPane_activity.children.clear()
+
+        for ((row, model) in activities.withIndex()) {
+            val fxmlLoader = FXMLLoader(TaycknerApplication::class.java.getResource("view/day_tracker/item_activity.fxml"))
+            val root: HBox = fxmlLoader.load()
+
+            val itemController = fxmlLoader.getController<ItemActivityController>()
+            itemController.set(model)
+            itemController.subscribe(this.activityListener)
+
+            gridPane_activity.add(root, 1, row)
+            gridPane_activity.minWidth = Region.USE_COMPUTED_SIZE
+            gridPane_activity.minHeight = Region.USE_COMPUTED_SIZE
+            gridPane_activity.maxWidth = Region.USE_COMPUTED_SIZE
+            gridPane_activity.maxHeight = Region.USE_COMPUTED_SIZE
+            gridPane_activity.prefWidth = Region.USE_COMPUTED_SIZE
+            gridPane_activity.prefHeight = Region.USE_COMPUTED_SIZE
+            GridPane.setMargin(root, Insets(10.0))
         }
     }
 
