69534279090b6e9e287bd3200f42176389632425
0x41gawor <41gawor@gmail.com>
2022-03-07 19:59:01 +0100

    feat(controller): make TaycknerApplication a NavigationSubscriber #41

--- src/main/kotlin/pl/gawor/taycknerdesktopclient/TaycknerApplication.kt
+++ src/main/kotlin/pl/gawor/taycknerdesktopclient/TaycknerApplication.kt
@@ -2,13 +2,32 @@ package pl.gawor.taycknerdesktopclient
 
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
+import pl.gawor.taycknerdesktopclient.controller.Observer.INavigationSubscriber
+import pl.gawor.taycknerdesktopclient.controller.daytracker.DayTrackerController
 
 class TaycknerApplication : Application() {
+
+    private val listener = object : INavigationSubscriber {
+        override fun button_dayPlannerOnAction() {
+            println("day planner")
+        }
+        override fun button_DayTrackerOnAction() {
+            println("day tracker")
+        }
+        override fun button_habitTrackerOnAction() {
+            println("habit tracker")
+        }
+    }
+
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(TaycknerApplication::class.java.getResource("view/day_planner/view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 1920.0, 1013.0)
+        val fxmlLoader = FXMLLoader(TaycknerApplication::class.java.getResource("view/day_tracker/view.fxml"))
+        val root = fxmlLoader.load<Parent>()
+        val dayTrackerController: DayTrackerController = fxmlLoader.getController() as DayTrackerController
+        dayTrackerController.subscribe(listener)
+        val scene = Scene(root, 1920.0, 1013.0)
         stage.title = "Tayckner"
         stage.scene = scene
         stage.show()
